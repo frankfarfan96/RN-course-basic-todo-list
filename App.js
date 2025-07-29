8654baacba503d5b5cd05464f75cb44021613d04
frank <frankfarfan96@gmail.com>
2025-07-29 16:51:22 +0200

    added principals components view for it rendering with style and 2 functions to handle the todo list

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enterdGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enterdGoalText);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContenier}> 
+      <View style={styles.inputConteiner}> 
+        <TextInput 
+            style={styles.textInput}
+            placeholder="Your course goal!"
+            onChangeText={goalInputHandler} />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals... </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+appContenier: { 
+  flex: 1,
+  padding: 50,
+  paddingHorizontal: 16,
+},
+inputConteiner: {
+  flex: 1,
+  flexDirection:"row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  marginBottom: 24,
+  borderBottomWidth: 1,
+  borderBottomColor: "#ccc",
+},
+textInput: {
+  borderWidth: 1,
+  borderColor: "#ccc",
+  width: "70%",
+  marginRight: 8
+},
+goalsContainer: {
+  flex: 5,
+}
+
 });
